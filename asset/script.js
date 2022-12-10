62ffaed19f2d93e610ab56edd0d89785b6c17b93
ryana <ryanananda.april@gmail.com>
2022-12-10 21:38:47 +0700

    second commit

---
+++ asset/script.js
@@ -0,0 +1,31 @@
+
+var swiper = new Swiper(".mySwiper", {
+    speed: 850,
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  $(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+  
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
+
+
